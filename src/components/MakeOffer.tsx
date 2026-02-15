5b308d01851c827ecfcabdc6ccde48375770931e
Ajmal <ajmalkkt@yahoo.com>
2026-02-15 16:22:17 +0530

    changing label name in approve offer page

--- src/components/MakeOffer.tsx
+++ src/components/MakeOffer.tsx
@@ -190,7 +190,7 @@ export default function MakeOfferOnline() {
                 : "text-gray-500 hover:text-gray-700"
                 }`}
             >
-              <Power size={14} /> Activate
+              <Power size={14} /> Inactive Offers
             </button>
             <button
               onClick={() => setActiveTab('deactivate')}
@@ -199,7 +199,7 @@ export default function MakeOfferOnline() {
                 : "text-gray-500 hover:text-gray-700"
                 }`}
             >
-              <XCircle size={14} /> Inactivate
+              <XCircle size={14} /> Active Offers
             </button>
           </div>
         </div>
